f174e49c3b8a8a8481f074f24b32a295bb220d84
thomas-negron <97264018+thomas-negron@users.noreply.github.com>
2022-01-19 19:21:34 -0500

    Finished First Bot

--- FirstBot.py
+++ FirstBot.py
@@ -4,7 +4,14 @@ from selenium.webdriver.common.by import By
 import time
 import openpyxl
 from selenium.webdriver.common.keys import Keys
+from openpyxl.styles import PatternFill, Font
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
 
+"""
 # Ask user for month and year
 inputYear = input("Year: ")
 inputMonth = input("Month: ")
@@ -91,6 +98,7 @@ time.sleep(15)
 
 # Close window when done
 myDriver.quit()
+"""
 
 # Workbook object
 wb_obj = openpyxl.load_workbook("/Users/richardthomas/Downloads/ProofOfCoverageReport.xlsx")
@@ -98,6 +106,61 @@ wb_obj = openpyxl.load_workbook("/Users/richardthomas/Downloads/ProofOfCoverageR
 # Sheet object created
 sheet_obj = wb_obj.active
 
-# Test loop to see if I can add elements to the workbook
-for i in range(22, 29):
-    sheet_obj.cell(row=1, column=i).value = "Agent"
+# Assign titles to 6 rows
+sheet_obj.cell(row=1, column=22).value = "Agent Name"
+sheet_obj.cell(row=1, column=23).value = "Contact Name"
+sheet_obj.cell(row=1, column=24).value = "Contact Title"
+sheet_obj.cell(row=1, column=25).value = "Contact Email"
+sheet_obj.cell(row=1, column=26).value = "Contact Phone"
+sheet_obj.cell(row=1, column=27).value = "Comments"
+
+sheet_obj['V1'].font = Font(size=11, bold=True)
+sheet_obj['V1'].fill = PatternFill(fill_type='solid', start_color='ffff00', end_color='ffff00')
+
+sheet_obj['W1'].font = Font(size=11, bold=True)
+sheet_obj['W1'].fill = PatternFill(fill_type='solid', start_color='ffff00', end_color='ffff00')
+
+sheet_obj['X1'].font = Font(size=11, bold=True)
+sheet_obj['X1'].fill = PatternFill(fill_type='solid', start_color='ffff00', end_color='ffff00')
+
+sheet_obj['Y1'].font = Font(size=11, bold=True)
+sheet_obj['Y1'].fill = PatternFill(fill_type='solid', start_color='ffff00', end_color='ffff00')
+
+sheet_obj['Z1'].font = Font(size=11, bold=True)
+sheet_obj['Z1'].fill = PatternFill(fill_type='solid', start_color='ffff00', end_color='ffff00')
+
+sheet_obj['AA1'].font = Font(size=11, bold=True)
+sheet_obj['AA1'].fill = PatternFill(fill_type='solid', start_color='ffff00', end_color='ffff00')
+
+wb_obj.save("YYYY_MM_JUA_LEADS.xlsx")
+
+email_user = 'rthomasnegron@gmail.com'
+email_password = 'onltkofdrfwxkrww'
+email_send = 'richnegy@gmail.com'
+
+subject = 'First bot test'
+
+msg = MIMEMultipart()
+msg['From'] = email_user
+msg['To'] = email_send
+msg['Subject'] = subject
+
+body = 'Hi there, sending this email from Python!'
+msg.attach(MIMEText(body, 'plain'))
+
+filename = 'YYYY_MM_JUA_LEADS.xlsx'
+attachment = open(filename, 'rb')
+
+part = MIMEBase('application', 'octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', "attachment; filename= " + filename)
+
+msg.attach(part)
+text = msg.as_string()
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(email_user, email_password)
+
+server.sendmail(email_user, email_send, text)
+server.quit()
